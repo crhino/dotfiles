7f3e5bc068c0226e928f90210a84e9948140370e
crhino <piraino.chris@gmail.com>
2014-08-14 22:45:23 -0400

    Add ./vim directory to source control

--- setup.sh
+++ setup.sh
@@ -3,4 +3,5 @@ ln -s ~/dotfiles/.vimrc ~/.vimrc
 ln -s ~/dotfiles/.git-prompt.sh ~/.git-prompt.sh
 ln -s ~/dotfiles/.gitconfig ~/.gitconfig
 ln -s ~/dotfiles/..bashrc ~/..bashrc
-ln -s ~/dotfiles/..bash_profile ~/..bash_profile
+ln -s ~/dotfiles/.bash_profile ~/.bash_profile
+ln -s ~/dotfiles/.vim ~/.vim
